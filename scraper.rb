2a589079d4e746fd6ba10b05aa2fb211e68492d5
Nick Kavadias <nick@kavadias.org>
2015-10-25 14:31:50 +1100

    inital commit. working RFS scraping

--- scraper.rb
+++ scraper.rb
@@ -1,19 +1,28 @@
 # This is a template for a Ruby scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# require 'scraperwiki'
-# require 'mechanize'
+require 'scraperwiki'
+require 'mechanize'
 #
-# agent = Mechanize.new
+agent = Mechanize.new
 #
+#puts "hello world"
 # # Read in a page
-# page = agent.get("http://foo.com")
+page = agent.get("http://www.rfs.nsw.gov.au/fire-information/fdr-and-tobans")
 #
 # # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
+ #p page.at(".danger-ratings-table").search("tr").first
+page.at(".danger-ratings-table tbody").search("tr").each do | row |
+  area 	              = row.search("td")[0].text
+  today_danger         = row.search("td")[1].text
+  today_ban        	  = row.search("td")[2].text
+  tomorrow_danger      = row.search("td")[3].text
+  tomorrow_ban         = row.search("td")[4].text
+  councils_affected    = row.search("td")[5].text
+  puts  area +" "+ " " +today_danger + " "+ today_ban +" " + tomorrow_ban + " " + councils_affected
+end
 # # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
+# ScraperWiki.save_sqlite( ["name"], {"name" => "susan", "occupation" => "software developer"})
 #
 # # An arbitrary query against the database
 # ScraperWiki.select("* from data where 'name'='peter'")
